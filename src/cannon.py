60c54078fbb5dbe51ad34b8dec2550d636872fed
Georgi Dimov <gt_dimov@mail.bg>
2015-07-09 19:04:21 +0300

    Add width and height in class Cannon.

--- src/cannon.py
+++ src/cannon.py
@@ -4,9 +4,42 @@ from src.projectile import Projectile
 class Cannon:
     def __init__(self, position, angle, initial_speed):
         self.position = position
+        self.height = 100
+        self.width = 100
+
         self.angle = angle
         self.initial_speed = initial_speed
 
     def fire(self):
         projectile = Projectile(self.position, self.angle, self.initial_speed)
         return projectile.get_coordinates()
+
+    def set_horizontal_position(self, x):
+        self.position.set_horizontal_position(x)
+
+    def get_horizontal_position(self):
+        return self.position.get_horizontal_position()
+
+    def set_vertical_position(self, y):
+        self.position.set_vertical_position(y)
+
+    def get_vertical_position(self):
+        return self.position.get_vertical_position()
+
+    def get_height(self):
+        return self.height
+
+    def get_width(self):
+        return self.width
+
+    def set_angle(self, new_angle):
+        self.angle = new_angle
+
+    def get_angle(self):
+        return self.angle
+
+    def set_initial_speed(self, new_speed):
+        self.initial_speed = new_speed
+
+    def get_initial_speed(self):
+        return self.initial_speed
